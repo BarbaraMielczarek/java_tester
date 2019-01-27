ef85f1e9acaa7a4ed656f9b28b2200b47d3cd12d
BarbaraMielczarek <46875769+BarbaraMielczarek@users.noreply.github.com>
2019-01-27 19:47:27 +0100

    Added a few functions

--- sandbox/src/main/java/ru/stqa/tester/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/tester/sandbox/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.tester.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
 
-    System.out.println("Hello, World!");
+    hello("World");
+    hello("User");
+    hello("Barbara");
 
+    double len = 5;
+    System.out.println("Powierzchnia kwadratu o boku " + len + " = " + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Powierzchnia prostokata o bokach " + a + " i " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a - b;
   }
 }
