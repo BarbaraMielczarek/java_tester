1dcb9778bdf40e356156dc73e915406679553a8a
BarbaraMielczarek <46875769+BarbaraMielczarek@users.noreply.github.com>
2019-02-19 01:19:26 +0100

    Task. 10 Implementation of verification tests, route of hijacking; Sort collections were completed in group and contact tests

--- addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/model/ContactData.java
@@ -14,8 +14,9 @@ public class ContactData {
   private final String email;
   private String group;
 
-  public ContactData( String firstname, String lastname, String title, String company, String companyAddress, String mobile, String workPhone, String email, String group) {
-    this.id = 0;
+
+  public ContactData(String firstname, String lastname, String title, String company, String companyAddress, String mobile, String workPhone, String email, String group) {
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.title = title;
@@ -97,13 +98,12 @@ public class ContactData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     ContactData that = (ContactData) o;
-    return id == that.id &&
-            Objects.equals(firstname, that.firstname) &&
+    return Objects.equals(firstname, that.firstname) &&
             Objects.equals(lastname, that.lastname);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, firstname, lastname);
+    return Objects.hash(firstname, lastname);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/tests/GroupModificationTests.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.tester.addressbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -22,7 +22,7 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test10", "testM10", "testM10a");
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test10", "testM10", "testM10a");
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
@@ -32,6 +32,9 @@ public class GroupModificationTests extends TestBase {
 
     before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
