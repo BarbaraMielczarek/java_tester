adfb78427854127c3d2ba258254c887ee76e0eb6
BarbaraMielczarek <46875769+BarbaraMielczarek@users.noreply.github.com>
2019-03-12 23:16:35 +0100

    Database driver, obtaining the information about Contacts from from BD

--- addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/tests/DbConnectionTest.java
@@ -1,6 +1,8 @@
 package ru.stqa.tester.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.tester.addressbook.model.ContactData;
+import ru.stqa.tester.addressbook.model.Contacts;
 import ru.stqa.tester.addressbook.model.GroupData;
 import ru.stqa.tester.addressbook.model.Groups;
 
@@ -9,7 +11,7 @@ import java.sql.*;
 public class DbConnectionTest {
 
   @Test
-  public void testDbConnection(){
+  public void testDbConnectionGroups(){
     Connection conn = null;
 
     try {
@@ -25,6 +27,35 @@ public class DbConnectionTest {
       st.close();
       conn.close();
       System.out.println(groups);
+
+      // Do something with the Connection
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+
+  }
+  @Test
+  public void testDbConnectionContacts(){
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select id, firstname, lastname, company, title, address, home, mobile,  work, email, email2, email3 from addressbook");
+      Contacts contacts = new Contacts();
+      while (rs.next()){
+        contacts.add (new ContactData().withId(rs.getInt("id")).withFirstname(rs.getString("firstname")).withLastname(rs.getString("lastname"))
+                .withCompany(rs.getString("company")).withTitle(rs.getString("title")).withCompanyAddress(rs.getString("address"))
+                .withHomePhone(rs.getString("home")).withMobilePhone(rs.getString("mobile")).withWorkPhone(rs.getString("work"))
+                .withEmail(rs.getString("email")).withEmail2(rs.getString("email2")).withEmail3(rs.getString("email3")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(contacts);
 
       // Do something with the Connection
     } catch (SQLException ex) {
