8167a0043c9cfd297c8eacb5869951d935cfd60f
BarbaraMielczarek <46875769+BarbaraMielczarek@users.noreply.github.com>
2019-03-14 21:41:04 +0100

    Switchable UI tests for  tests of Groups

--- addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/tester/addressbook/tests/TestBase.java
@@ -9,9 +9,16 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.tester.addressbook.appmanager.ApplicationManager;
+import ru.stqa.tester.addressbook.model.GroupData;
+import ru.stqa.tester.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class TestBase {
   Logger loger = LoggerFactory.getLogger(GroupCreationTests.class);
@@ -39,6 +46,15 @@ public class TestBase {
   public void logTestStop(Method m) {
     loger.info("Stop test " + m.getName());
 
+  }
+  public void verifyGroupListInUi() {
+    if(Boolean.getBoolean("verifyUI")){
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
   }
 
 }
