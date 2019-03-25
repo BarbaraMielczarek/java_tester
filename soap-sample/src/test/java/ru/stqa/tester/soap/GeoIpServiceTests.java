d89067acd334b2de33f247e5c9e94862ddfda843
BarbaraMielczarek <46875769+BarbaraMielczarek@users.noreply.github.com>
2019-03-25 22:48:39 +0100

    Preparation for SOAP part 2

---
+++ soap-sample/src/test/java/ru/stqa/tester/soap/GeoIpServiceTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.tester.soap;
+
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("83.7.176.216");
+    assertEquals(geoIP.getCountryCode(), "PL");
+
+  }
+}
